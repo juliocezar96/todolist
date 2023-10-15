c9c0c2ff1e80d263c2276bc418d1bec9e08982e1
Julio Cezar <julio.cezar@liferay.com>
2023-10-15 19:50:48 -0300

    getId Null

--- src/main/java/br/com/juliocezar/todolist/task/TaskController.java
+++ src/main/java/br/com/juliocezar/todolist/task/TaskController.java
@@ -1,10 +1,13 @@
 package br.com.juliocezar.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,7 +17,10 @@ public class TaskController {
     private ITaskRepository iTaskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel){
+    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+        var idUser = request.getAttribute("idUser");
+        taskModel.setIdUser((UUID) idUser);
+        System.out.println(taskModel);
         var task = this.iTaskRepository.save(taskModel);
         return task;
 
